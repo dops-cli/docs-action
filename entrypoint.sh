b3f480d9f0bc29553c96585ca049931fc9b5f855
MarvinJWendt <git@marvinjwendt.com>
2020-08-13 15:39:56 +0200

    fix: add missing files

---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+
+echo "## Initializing git repo..."
+git init
+echo "### Adding git remote..."
+git remote add origin https://x-access-token:$ACCESS_TOKEN@github.com/$REPO_FULLNAME.git
+echo "### Getting branch"
+BRANCH=${GITHUB_REF#*refs/heads/}
+
+if [[ $BRANCH == refs/tags* ]]; then
+  echo "## The push was a tag, aborting!"
+  exit
+fi
+
+echo "### git fetch $BRANCH ..."
+git fetch origin $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+git checkout $BRANCH
+
+echo "## Login into git..."
+git config --global user.email "git@marvinjwendt.com"
+git config --global user.name "MarvinJWendt"
+
+echo "## Ignore workflow files (we may not touch them)"
+git update-index --assume-unchanged .github/workflows/*
+
+# Start release
+
+go get -u
+
+FEATURE_COUNT=$(go run . mods --count)
+
+sed -i -E -r 's/`.*`<!-- feature-count -->/`'"$FEATURE_COUNT"'`<!-- feature-count -->/g' README.md
+
+echo "## Staging changes..."
+git add .
+echo "## Commiting files..."
+git commit -m "ci: release new version" || true
+echo "## Pushing to $BRANCH"
+git push -u origin $BRANCH
